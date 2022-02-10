1c864d39143746a9130097e09b0c5aa34529eca8
Damir Issayev <82366417+TheKoke@users.noreply.github.com>
2022-02-10 20:10:34 +0600

    Update Symmetric.cs

--- Matrices/Symmetric.cs
+++ Matrices/Symmetric.cs
@@ -1,34 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Drogergous.Matrices
 {
     class Symmetric : Single
     {
-        private int[,] SymmetricMatrix;
+        private int[,] Symmetrix;
         
-        private void SetMatrix(int[,] Matrix)
+        public Symmetric(int[,] Sample)
         {
-            Size = Matrix.GetLength(0);
-
-            SymmetricMatrix = GetSingle();
+            this.Size = Sample.GetLength(0);
+            Symmetrix = (int[,])Sample.Clone();
         }
 
-        private void SetDiagonal(int[,] Matrix)
+        public int[,] GetUpperMatrix()
         {
             for (int i = 0; i < Size; i++)
             {
-                SymmetricMatrix[i, i] = Matrix[i, i];
+                for (int i = 0; i < Size; i++)
+                {
+                    if (i - j <= Size - 1 && i - j > 0)
+                    {
+                        Symmetricx[i, j] == Symmetrix[j, i];
+                    }
+                }
             }
+            
+            return Symmetrix;
         }
-
-        public int[,] GetMatrix(int[,] Matrix)
+        
+        public int[,] GetLowerMatrix()
         {
-            SetMatrix(Matrix);
-            SetDiagonal(Matrix);
-
-            return SymmetricMatrix;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (j - i <= Size - 1 && j - i > 0)
+                    {
+                        Symmetrix[i, j] == Symmetrix[j, i];
+                    }
+                }
+            }
+            
+            return Symmetrix;
         }
     }
 }
