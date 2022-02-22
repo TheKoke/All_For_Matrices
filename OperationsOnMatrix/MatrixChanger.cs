f54ab3b8ce601807b645815c5f3c8cde358643b9
Damir Issayev <82366417+TheKoke@users.noreply.github.com>
2022-02-22 19:45:06 +0600

    Refactor of MatrixChanger.cs

--- OperationsOnMatrix/MatrixChanger.cs
+++ OperationsOnMatrix/MatrixChanger.cs
@@ -33,26 +33,22 @@ namespace Drogergous.OperationsOnMatrix
                 }
             }
 
-            Changed = TransMatrix(Changed);
-
-            return Changed;
+            return TransMatrix(Changed);
         }
 
         public static float[,] InverseMatrix(int[,] Matrix)
         {
             Changed = AddJointMatrix(Matrix);
 
-            float[,] Inverse = new float[Matrix.GetLength(0), Matrix.GetLength(1)];
-
             for (int i = 0; i < Matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < Matrix.GetLength(0); j++)
                 {
-                    Inverse[i, j] = (float)Changed[i, j] / Determinants.Resolve(Matrix);
+                    (float)Changed[i, j] /= Determinants.Resolve(Matrix);
                 }
             }
 
-            return Inverse;
+            return Changed;
         }
     }
 }
