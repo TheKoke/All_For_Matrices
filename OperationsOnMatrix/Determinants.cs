04634b28fc78be93772bf5b468f309fdf17cb323
Damir Issayev <82366417+TheKoke@users.noreply.github.com>
2022-03-04 22:02:25 +0600

    Refactor (add new overload method) Determinant.cs

--- OperationsOnMatrix/Determinants.cs
+++ OperationsOnMatrix/Determinants.cs
@@ -4,49 +4,45 @@ namespace Drogergous.OperationsOnMatrix
 {
     class Determinants
     {
-        public static int Second_Order(int[,] Matrix)
-        { 
-            int positive = 1;
-            int negative = 1;
-
-            for (int i = 0; i < Matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < Matrix.GetLength(0); j++)
-                {
-                    if (i == j)
-                    {
-                        positive *= Matrix[i, j];
-                    }
-                    else
-                    {
-                        negative *= Matrix[i, j];
-                    }
-                }
-            }
-
-            return positive - negative;
-        }
-
         public static int Resolve(int[,] Matrix)
         {
-            if (Matrix.GetLength(0) == 2)
+            if (Matrix.GetLength(0) != Matrix.GetLength(1))
             {
-                return Second_Order(Matrix);
+                return int.MinValue;
+            }
+            
+            if (Matrix.GetLength(0) == 1)
+            {
+                return Matrix[0, 0];
             }
             
             int result = 0;
             for (int i = 0; i < Matrix.GetLength(0); i++)
             {
-                if (i % 2 == 0)
-                {
-                    result += Matrix[0, i] * Resolve(MatrixAlgebra.GetMinor(Matrix, 0, i));
-                }
-                else
-                {
-                    result -= Matrix[0, i] * Resolve(MatrixAlgebra.GetMinor(Matrix, 0, i));
-                }
+                result += Matrix[0, i] * MatrixAlgebra.AlgebraicApp(Matrix, 0, i);
             }
 
+            return result;
+        }
+        
+        public static float Resolve(float[,] Matrix)
+        {
+            if (Matrix.GetLength(0) != Matrix.GetLength(1))
+            {
+                return float.MinValue;
+            }
+            
+            if (Matrix.GetLength(0) == 1)
+            {
+                return Matrix[0, 0];
+            }
+            
+            float result = 0;
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                result += Matrix[0, i] * MatrixAlgebra.AlgebraicApp(Matrix, 0, i);
+            }
+            
             return result;
         }
     }
